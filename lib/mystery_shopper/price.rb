360b78bbc8ea56fb1ddd02e4959ed005cacccc69
Hunter Braun <hunter.braun@gmail.com>
2018-07-22 22:18:27 -0500

    Add Price class

---
+++ lib/mystery_shopper/price.rb
@@ -0,0 +1,41 @@
+require 'date'
+
+module MysteryShopper
+  class Price
+    def initialize(data)
+      @data = data
+    end
+
+    def title_id
+      @title_id ||= data.fetch('title_id')
+    end
+
+    def sales_status
+      @sales_status ||= data.fetch('sales_status')
+    end
+
+    def regular_price
+      @regular_price ||= data.dig('regular_price', 'raw_value').to_f
+    end
+
+    def discount_price
+      @discount_price ||= discount.fetch('raw_value').to_f
+    end
+
+    def sale_start
+      @sale_start ||= Date.parse(discount.fetch('start_datetime'))
+    end
+
+    def sale_end
+      @sale_end ||= Date.parse(discount.fetch('end_datetime'))
+    end
+
+    private
+
+    attr_reader :data
+
+    def discount
+      @discount ||= data.fetch('discount_price')
+    end
+  end
+end
