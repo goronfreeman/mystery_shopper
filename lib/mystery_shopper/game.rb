f724c99d73c734c2606614ac9597c1477a4c1cc1
Hunter Braun <hunter.braun@gmail.com>
2019-04-24 17:57:43 -0500

    Add new methods for new JSON response

--- lib/mystery_shopper/game.rb
+++ lib/mystery_shopper/game.rb
@@ -8,52 +8,97 @@ module MysteryShopper
       @data = data
     end
 
-    def categories
-      @categories ||= data.fetch('categories')
-    end
-
     def url
       @url ||= "#{base_url}#{data.fetch('url')}"
-    end
-
-    def release_date
-      @release_date ||= Date.parse(data.fetch('releaseDateMask'))
-    end
-
-    def free_to_start?
-      @free_to_start ||= data.fetch('freeToStart')
     end
 
     def title
       @title ||= data.fetch('title')
     end
 
-    def slug
-      @slug ||= data.fetch('slug')
-    end
-
-    def platform
-      @platform ||= data.fetch('platform')
+    def description
+      @description ||= data.fetch('description')
     end
 
     def id
       @id ||= data.fetch('id')
     end
 
-    def number_of_players
-      @number_of_players ||= data.fetch('players')
-    end
-
     def nsuid
       @nsuid ||= data['nsuid']
+    end
+
+    def slug
+      @slug ||= data.fetch('slug')
     end
 
     def front_box_art
       @front_box_art ||= "#{base_url}#{data.fetch('boxArt')}"
     end
 
-    def buy_online?
-      @buy_online ||= data.fetch('buyonline')
+    # TODO: Figure out what the value  returned here actually is.
+    def gallery
+      @gallery ||= data.fetch('gallery')
+    end
+
+    def platform
+      @platform ||= data.fetch('platform')
+    end
+
+    def release_date
+      @release_date ||= Date.parse(data.fetch('releaseDateMask'))
+    end
+
+    def characters
+      @characters ||= data.fetch('characters')
+    end
+
+    def categories
+      @categories ||= data.fetch('categories')
+    end
+
+    def esrb
+      @esrb ||= data.fetch('esrb')
+    end
+
+    def esrb_descriptors
+      @esrb_descriptors ||= data.fetch('esrbDescriptors')
+    end
+
+    def virtual_console
+      @virtual_console ||= data.fetch('virtualConsole')
+    end
+
+    def general_filters
+      @general_filters ||= data.fetch('generalFilters')
+    end
+
+    def filter_shops
+      @filter_shops ||= data.fetch('filterShops')
+    end
+
+    def filter_players
+      @filter_players ||= data.fetch('filterPlayers')
+    end
+
+    def number_of_players
+      @number_of_players ||= data.fetch('players')
+    end
+
+    def featured?
+      @featured ||= data.fetch('featured')
+    end
+
+    def free_to_start?
+      @free_to_start ||= data.fetch('freeToStart')
+    end
+
+    def price_range
+      @price_range ||= data.fetch('priceRange')
+    end
+
+    def availability
+      @availability ||= data.fetch('availability')
     end
 
     def regular_price
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe MysteryShopper::Game do
       'gallery' => '1nNjVtaDE6yijrWKfJL5ebXMPSh-mlaS',
       'platform' => 'Nintendo Switch',
       'releaseDateMask' => '2018-08-06T00:00:00.000-07:00',
-      'characters' => [],
+      'characters' => ['Mario'],
       'categories' => %w[Strategy Party Simulation],
       'msrp' => 24.99,
       'salePrice' => 18.74,
@@ -60,21 +60,136 @@ RSpec.describe MysteryShopper::Game do
     )
   end
 
-  describe '#categories' do
-    it 'returns an array of categories' do
-      expect(subject.categories).to eq %w[Strategy Party Simulation]
-    end
-  end
-
   describe '#url' do
     it 'returns the correct URL' do
       expect(subject.url).to eq 'https://www.nintendo.com/games/detail/overcooked-2-switch'
     end
   end
 
+  describe '#title' do
+    it 'returns the game title' do
+      expect(subject.title).to eq 'Overcooked! 2'
+    end
+  end
+
+  describe '#description' do
+    it 'returns the game description' do
+      expect(subject.description).to eq 'Sample description.'
+    end
+  end
+
+  describe '#id' do
+    it 'returns the game ID' do
+      expect(subject.id).to eq 'bMo0R6zC5D5ewf5wTc424zc7RQnDFSIp'
+    end
+  end
+
+  describe '#nsuid' do
+    it 'returns the game NSUID' do
+      expect(subject.nsuid).to eq '70010000003402'
+    end
+  end
+
+  describe '#slug' do
+    it 'returns the game slug' do
+      expect(subject.slug).to eq 'overcooked-2-switch'
+    end
+  end
+
+  describe '#front_box_art' do
+    it 'returns the URL of the game box art' do
+      expect(subject.front_box_art).to eq 'https://www.nintendo.com/content/dam/noa/en_US/games/switch/o/overcooked-2-switch/Switch_Overcooked2_box.png'
+    end
+  end
+
+  describe '#gallery' do
+    it 'returns the gallery' do
+      expect(subject.gallery).to eq '1nNjVtaDE6yijrWKfJL5ebXMPSh-mlaS'
+    end
+  end
+
+  describe '#platform' do
+    it 'returns the game platform' do
+      expect(subject.platform).to eq 'Nintendo Switch'
+    end
+  end
+
   describe '#release_date' do
     it 'returns the correct Date object' do
       expect(subject.release_date).to eq Date.new(2018, 8, 6)
+    end
+  end
+
+  describe '#characters' do
+    it 'returns an array of characters' do
+      expect(subject.characters).to eq %w[Mario]
+    end
+  end
+
+  describe '#categories' do
+    it 'returns an array of categories' do
+      expect(subject.categories).to eq %w[Strategy Party Simulation]
+    end
+  end
+
+  describe '#esrb' do
+    it 'returns the ESRB rating' do
+      expect(subject.esrb).to eq 'Everyone'
+    end
+  end
+
+  describe '#esrb_descriptors' do
+    it 'returns an array of ESRB descriptors' do
+      expect(subject.esrb_descriptors).to eq ['Users Interact']
+    end
+  end
+
+  # This appears to be "na" for every result right now.
+  describe '#virtual_console' do
+    it 'returns "na"' do
+      expect(subject.virtual_console).to eq 'na'
+    end
+  end
+
+  describe '#general_filters' do
+    it 'returns an array of general filters that match the game' do
+      expect(subject.general_filters).to eq ['DLC available', 'Online Play via Nintendo Switch Online', 'Deals']
+    end
+  end
+
+  describe '#filter_shops' do
+    it 'returns an array of shop filters that match the game' do
+      expect(subject.filter_shops).to eq ['At retail', 'On Nintendo.com']
+    end
+  end
+
+  describe '#filter_players' do
+    it 'returns an array of player filters that match the game' do
+      expect(subject.filter_players).to eq ['3+', '2+', '1+']
+    end
+  end
+
+  describe '#number_of_players' do
+    it 'returns the number of players for the game' do
+      expect(subject.number_of_players).to eq 'up to 4 players'
+    end
+  end
+
+  describe '#featured?' do
+    context 'when it is true' do
+      it 'returns true' do
+        expect(subject.featured?).to be true
+      end
+    end
+
+    context 'when it is false' do
+      before do
+        data['featured'] = false
+      end
+
+      it 'returns false' do
+        expect(subject.featured?).to be false
+      end
     end
   end
 
@@ -96,45 +211,15 @@ RSpec.describe MysteryShopper::Game do
     end
   end
 
-  describe '#title' do
-    it 'returns the game title' do
-      expect(subject.title).to eq 'Overcooked! 2'
+  describe '#price_range' do
+    it 'returns the price range of the game' do
+      expect(subject.price_range).to eq '$10 - $19.99'
     end
   end
 
-  describe '#slug' do
-    it 'returns the game slug' do
-      expect(subject.slug).to eq 'overcooked-2-switch'
-    end
-  end
-
-  describe '#platform' do
-    it 'returns the game platform' do
-      expect(subject.platform).to eq 'Nintendo Switch'
-    end
-  end
-
-  describe '#id' do
-    it 'returns the game ID' do
-      expect(subject.id).to eq 'bMo0R6zC5D5ewf5wTc424zc7RQnDFSIp'
-    end
-  end
-
-  describe '#number_of_players' do
-    it 'returns the number of players for the game' do
-      expect(subject.number_of_players).to eq 'up to 4 players'
-    end
-  end
-
-  describe '#nsuid' do
-    it 'returns the game NSUID' do
-      expect(subject.nsuid).to eq '70010000003402'
-    end
-  end
-
-  describe '#front_box_art' do
-    it 'returns the URL of the game box art' do
-      expect(subject.front_box_art).to eq 'https://www.nintendo.com/content/dam/noa/en_US/games/switch/o/overcooked-2-switch/Switch_Overcooked2_box.png'
+  describe '#availability' do
+    it 'returns an array of availability statuses' do
+      expect(subject.availability).to eq ['Available now']
     end
   end
 
